70033b25034a15b7a028205637db0d300ac981f2
Alan <freitasbr01@gmail.com>
2024-05-18 15:25:28 -0300

    update deploy

--- src/repositories/UserRepository.js
+++ src/repositories/UserRepository.js
@@ -2,7 +2,7 @@
 const sqliteConnection = require('../database/sqlite');
 
 class UserRepository {
-  async findByEmail(email) {
+  async findByEmail({email}) {
     const database = await sqliteConnection();
     const user = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
 
